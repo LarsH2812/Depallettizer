bd2263f0af1f8bb249246057fa7efb46cdc313c9
LarsH01 <20097794@student.hhs.nl>
2022-09-23 15:00:56 +0200

    box recognition is reliable

--- IntelRealsence/imagecontrol.py
+++ IntelRealsence/imagecontrol.py
@@ -1,22 +1,66 @@
 import cv2 as cv
 import numpy as np
+import imutils
 import time
 
+def click_event(event, x, y, flags, params):
+    if event == cv.EVENT_LBUTTONDOWN:
+
+        cv.putText(boxes, f"{x}, {y}", (x,y), cv.FONT_HERSHEY_SIMPLEX,1,(255,255,255))
+        cv.imshow('out', boxes)
+
+
 boxes = cv.imread("boxes.png")
-cv.imshow('boxes',  boxes)
+# cv.imshow('boxes',  boxes)
 
 gray = cv.cvtColor(boxes, cv.COLOR_BGR2GRAY)
 
-_, bw = cv.threshold(gray, 105, 255, cv.THRESH_BINARY_INV)
-
-contrours, hierarchy = cv.findContours(bw, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-
-cv.drawContours(boxes, contrours, -1,(255,0,0), 2,cv.LINE_AA )
+_,bw = cv.threshold(gray, 105, 255, cv.THRESH_BINARY)
 
 
-cv.imshow('gray',   gray )
-cv.imshow('bw',     bw   )
+contours = cv.findContours(bw, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+contours = imutils.grab_contours(contours)
+
+check = boxes.copy()
+cv.drawContours(check, contours, -1,(255,0,0), 2,cv.LINE_AA)
+cv.imshow("check", check)
+
+for c in contours:
+    try:
+        M = cv.moments(c)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        
+
+        rect = cv.minAreaRect(c)
+        box = cv.boxPoints(rect)
+        box = np.int0(box)
+
+        length, width = max(rect[1]), min(rect[1])
+        area = length * width
+
+        print(f"{area} v.s. {M['m00']}")
+
+        angle = rect[-1]
+
+        if length >= 260 and width >= 93:
+            cv.drawContours(boxes, [box], -1,(255,0,0), 2,cv.LINE_AA)
+            cv.circle(boxes,(cX,cY), 7, (255,255,255), -1)
+            cv.putText(boxes, "CENTER", (cX-20, cY+20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255),2)
+            cv.putText(boxes, f"{format(angle, '.2f')}", (cX-20, cY+40), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255),2)
+
+    except:
+        cv.drawContours(boxes, [c], -1,(0,0,255), 2,cv.LINE_AA )
+
+
+
+
+
+# cv.imshow('gray',   gray )
+# cv.imshow('bw',     bw   )
 cv.imshow('out',    boxes)
+
+cv.setMouseCallback('out',click_event)
 
 
 
--- IntelRealsence/main.py
+++ IntelRealsence/main.py
@@ -1,19 +1,23 @@
-## License: Apache 2.0. See LICENSE file in root directory.
-## Copyright(c) 2015-2017 Intel Corporation. All Rights Reserved.
+#############################################
+### autor: Lars Hartman                   ###
+#############################################
 
-###############################################
-##      Open CV and Numpy integration        ##
-###############################################
-
+from math import sin, cos, radians
+from time import sleep
 import pyrealsense2 as rs
 import numpy as np
 import cv2 as cv
+import imutils
+
+cnts = None
+
+framesize = {'width':1280, 'height': 720}
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
 
-videocapture = cv.VideoWriter("ContourDetected.avi",  cv.VideoWriter_fourcc('M','J','P','G'), 30, (1280,960))
+# videocapture = cv.VideoWriter("ContourDetected.avi",  cv.VideoWriter_fourcc('M','J','P','G'), 30, (1280,960))
 
 # Get device product line for setting a supporting resolution
 pipeline_wrapper = rs.pipeline_wrapper(pipeline)
@@ -30,12 +34,12 @@ if not found_rgb:
     print("The demo requires Depth camera with Color sensor")
     exit(0)
 
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.depth, framesize["width"], framesize["height"], rs.format.z16, 30)
 
 if device_product_line == 'L500':
     config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
 else:
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    config.enable_stream(rs.stream.color, framesize["width"], framesize["height"], rs.format.bgr8, 30)
 
 # Start streaming
 profile = pipeline.start(config)
@@ -44,52 +48,17 @@ depthSensor = profile.get_device().first_depth_sensor()
 depthScale  = depthSensor.get_depth_scale()
 print(f"Depth Scale is: {depthScale}")
 
-maxDistMeters = 1.65 #[m]
+maxDistMeters = 1.63 #[m]
 maxDist = maxDistMeters / depthScale
 
 align_to = rs.stream.color
 align = rs.align(align_to)
 
+i = 0
+cv.namedWindow('RealSense', cv.WINDOW_AUTOSIZE)
+
 try:
     while True:
-
-        # Wait for a coherent pair of frames: depth and color
-        frames = pipeline.wait_for_frames()
-
-        aligned_frames = align.process(frames)
-
-        alignedDepthFrame = aligned_frames.get_depth_frame()
-        color_frame = aligned_frames.get_color_frame()
-
-        if not alignedDepthFrame or not color_frame:
-            continue
-
-        depthImage = np.asanyarray(alignedDepthFrame.get_data())
-        colorImage = np.asanyarray(color_frame.get_data())
-
-        gray_color = 0
-        white_color = 0
-        depthImage3D = np.dstack((depthImage,depthImage,depthImage))
-        bgRemoved = np.where((depthImage3D > maxDist) | (depthImage3D <= 0), gray_color, colorImage)
-
-        depthColormap = cv.applyColorMap(cv.convertScaleAbs(depthImage, alpha=0.03), cv.COLORMAP_JET)
-
-        # Show images
-        cv.namedWindow('RealSense', cv.WINDOW_AUTOSIZE)
-
-        # cv.imshow('RealSense', bgRemoved)
-
-        gray = cv.cvtColor(bgRemoved, cv.COLOR_BGR2GRAY)
-        horizontal1 = np.hstack((bgRemoved,np.dstack((gray,gray,gray))))
-        _, bw = cv.threshold(gray, 105, 255, cv.THRESH_BINARY)
-        contours, _ = cv.findContours(bw, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-
-        cv.drawContours(bgRemoved, contours, -1, (0,255,0), 2, cv.LINE_AA)
-        horizontal2 = np.hstack((np.dstack((bw,bw,bw)), bgRemoved))
-        images = np.vstack((horizontal1,horizontal2))
-        
-        cv.imshow('RealSense', images)
-        videocapture.write(images)
 
         k = cv.waitKey(1)
 
@@ -97,9 +66,82 @@ try:
             break
         elif k == ord('s'):
             cv.imwrite("boxes.png",bgRemoved)
+        elif k == 32 :
+            
+
+            i = i+1
+            
+            # Wait for a coherent pair of frames: depth and color
+            frames = pipeline.wait_for_frames()
+
+            aligned_frames = align.process(frames)
+
+            alignedDepthFrame = aligned_frames.get_depth_frame()
+            color_frame = aligned_frames.get_color_frame()
+
+            if not alignedDepthFrame or not color_frame:
+                continue
+
+            depthImage = np.asanyarray(alignedDepthFrame.get_data())
+            colorImage = np.asanyarray(color_frame.get_data())
+
+            gray_color = 0
+            depthImage3D = np.dstack((depthImage,depthImage,depthImage))
+            bgRemoved = np.where((depthImage3D > maxDist) | (depthImage3D <= 0), gray_color, colorImage)
+
+            depthColormap = cv.applyColorMap(cv.convertScaleAbs(depthImage, alpha=0.03), cv.COLORMAP_JET)
+            horizontal1 = np.hstack((depthColormap,colorImage, bgRemoved))
+            # Show images
+            
+
+            # cv.imshow('RealSense', bgRemoved)
+
+            gray = cv.cvtColor(bgRemoved, cv.COLOR_BGR2GRAY)
+            gray = cv.GaussianBlur(gray, (5,5), 0)
+            _, bw = cv.threshold(gray, 96, 255, cv.THRESH_BINARY)
+            contours = cv.findContours(bw, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+            contours = imutils.grab_contours(contours)
+            for cont in contours:
+                try:
+            
+                    M = cv.moments(cont)
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+
+                    rect = cv.minAreaRect(cont)
+                    box = cv.boxPoints(rect)
+                    box = np.int0(box)
+
+                    length, width = max(rect[1]), min(rect[1])
+                    area = M["m00"]
+
+                    angle = rect[-1]
+
+                    if rect[1][0] < rect[1][1]:
+                        angle = angle 
+                    else:
+                        angle = angle + 90
+
+                    if area >= 40000:
+                        cv.drawContours(colorImage, [box], -1, (255,255,255), 2)
+                        cv.circle(colorImage, (cX,cY), 7, (255,255,255), -1)
+                        cv.putText(colorImage, f"CENTER:", (cX-20,cY+20),cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255),2)
+                        cv.putText(colorImage, f"({cX}, {cY})", (cX-20,cY+40),cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255),2)
+                        cv.putText(colorImage, f"({format(angle, '.2f')})", (cX-20,cY+60),cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255),2)
+
+                        lenArrow = 100
+                        cv.arrowedLine(colorImage, (cX,cY), (int(cX + lenArrow*cos(radians(angle-90))), int(cY + lenArrow*sin(radians(angle-90)))), (255,255,255), 2)
+
+                except:
+                    cv.drawContours(colorImage,[cont],-1,(0,0,255), 2)
+
+            horizontal2 = np.hstack((np.dstack((gray,gray,gray)),np.dstack((bw,bw,bw)), colorImage))
+            images = np.vstack((horizontal1,horizontal2))
+            
+            cv.imshow('RealSense', colorImage)
+            # videocapture.write(images)
 
 finally:
-    print(images.shape)
     # Stop streaming
     cv.destroyAllWindows()
     pipeline.stop()
