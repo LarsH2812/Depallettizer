1588f4b1b5d3b1f5f13fda04805132813b66ae44
LarsH01 <20097794@student.hhs.nl>
2022-10-06 16:40:39 +0200

    Detected ArUco marker

--- Aruco/aruco.py
+++ Aruco/aruco.py
@@ -25,6 +25,60 @@ ARUCO_DICT = {
 	"DICT_APRILTAG_36h11":  aruco.DICT_APRILTAG_36h11
 }
 
-img = cv.imread("Aruco/WIN_20221005_12_40_15_Pro.jpg")
-cv.imshow('Input', img)
-cv.waitKey(0)
+vid = cv.VideoCapture(0,cv.CAP_DSHOW)
+
+vid.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
+vid.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
+
+writer = cv.VideoWriter("ArUco.avi",  cv.VideoWriter_fourcc('M','J','P','G'), 30, (1280,720))
+capturing = False
+
+arucoDict = aruco.Dictionary_get(ARUCO_DICT["DICT_ARUCO_ORIGINAL"])
+arucoParams = aruco.DetectorParameters_create()
+
+cv.namedWindow("output",cv.WINDOW_AUTOSIZE)
+k = cv.waitKey(1)
+
+while (k != 27):
+	_, frame = vid.read()
+	(corners, ids, rejected) = aruco.detectMarkers(frame,arucoDict, parameters = arucoParams) 
+	if not ids is None:
+		ids = ids.flatten()
+
+		for (marekerCorner, markerID) in zip(corners, ids):
+			(topLeft, topRight, bottomRight, bottomLeft) = marekerCorner.reshape((4,2))
+
+			topRight = (int(topRight[0]), int(topRight[1]))
+			topLeft = (int(topLeft[0]), int(topLeft[1]))
+			bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+			bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+			topCenter = (int((topRight[0]+topLeft[0])/2), int((topRight[1]+topLeft[1])/2))
+
+			cX = int((topRight[0] + bottomLeft[0]) / 2)
+			cY = int((topRight[1] + bottomLeft[1]) / 2)
+
+			h = bottomRight[1] - topRight[1]
+			w = topRight[0] - topLeft[0]  
+
+			cv.line(frame, topRight, topLeft, (0,255,0), 1, cv.LINE_AA)
+			cv.line(frame, topLeft, bottomLeft, (0,255,0), 1, cv.LINE_AA)
+			cv.line(frame, bottomLeft, bottomRight, (0,255,0), 1, cv.LINE_AA)
+			cv.line(frame, bottomRight, topRight, (0,255,0), 1, cv.LINE_AA)
+			cv.putText(frame,f"ID: {markerID}", (cX+20, cY+20),cv.FONT_HERSHEY_TRIPLEX, 1, (0,255,0), 1, cv.LINE_AA)
+			# cv.putText(frame, f"Height = {h}",(cX +int(w/2), cY),cv.FONT_HERSHEY_SIMPLEX, 1,(0,255,0), 1, cv.LINE_AA)
+			# cv.putText(frame, f"Width = {w}",(cX-int(w/2) , cY+int(h/2)),cv.FONT_HERSHEY_SIMPLEX, 1,(0,255,0), 1, cv.LINE_AA)
+			cv.arrowedLine(frame, (cX, cY), topCenter, (0,255,0),1,cv.LINE_AA)
+
+			cv.circle(frame, (cX, cY),5,(0,255,0),-1,cv.LINE_AA)
+	
+	if capturing:
+		writer.write(frame)
+		cv.putText(frame, "Caputuring!", (20,50), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),1,cv.LINE_AA)
+	cv.imshow("output",frame)
+	k = cv.waitKey(1)
+	if k == 32:
+		capturing = True if not capturing else False
+
+
+cv.destroyAllWindows()
+vid.release()
