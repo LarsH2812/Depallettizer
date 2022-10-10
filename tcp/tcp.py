27e73574b764caeb2a6fff102375e67c010c7b33
LarsH01 <20097794@student.hhs.nl>
2022-10-10 14:33:34 +0200

    added some markup

--- tcp/tcp.py
+++ tcp/tcp.py
@@ -3,16 +3,15 @@ from time import sleep
 
 pcHostName = tcp.gethostname()
 hostIp = tcp.gethostbyname(pcHostName)
-hostPort = 49252
-serverAdress = (hostIp, hostPort)
+HOSTPORT = 9000
+serverAdress = (hostIp, HOSTPORT)
 
-robotIp = "198.162.0.1"
-PORT = 2000 #todo: change this
+robotIp = "198.162.0.5"
 
 print(f"""
 pc name:    {pcHostName},
 ip address: {hostIp},
-pc port:    {hostPort}
+pc port:    {HOSTPORT}
 """)
 
 tcpSocket = tcp.socket(tcp.AF_INET, tcp.SOCK_STREAM)
@@ -25,34 +24,34 @@ rot = 90
 ori = 0
 
 while True:
-    print(f"[*] Started listening on :{hostIp}:{hostPort}")
+    print(f"\u001b[34m [*] \u001b[0m Started listening on :{hostIp}:{HOSTPORT}")
     clientSocket, clientAddress = tcpSocket.accept()
 
-    print(f"[*] Got connection from {clientAddress[0]}:{clientAddress[1]}")
+    print(f"\u001b[34m [*] \u001b[0m Got connection from {clientAddress[0]}:{clientAddress[1]}")
 
     while True:
         data = clientSocket.recv(1024).decode()
-        print(f"[*] Received {data} from the client")
-        print("[*] Processing data")
+        print(f"\u001b[34m [*] \u001b[0m Received {data} from the client")
+        print("\u001b[34m [*] \u001b[0m Processing data")
         if(data == "hello server"):
             clientSocket.send("1".encode())
-            print("[*] Reply sent: 1")
+            print("\u001b[34m [*] \u001b[0m Reply sent: 1")
         elif(data=="close"):
             clientSocket.send("Goodbye".encode())
-            print("[*] Reply sent: Goodbye")
-            print("[*] Connection closed.")
+            print("\u001b[34m [*] \u001b[0m Reply sent: Goodbye")
+            print("\u001b[34m [*] \u001b[0m Connection closed.")
             clientSocket.close()
             break
         elif(data == "quit"):
             clientSocket.send("Server stopped".encode())
-            print("[*] Reply sent: Server Stopped")
+            print("\u001b[34m [*] \u001b[0m Reply sent: Server Stopped")
             clientSocket.close()
             break
         elif(data=="getData"):
             coords = f"{x},{y},{rot},{ori}"
             print(coords)
             clientSocket.send(coords.encode())
-            print(f"[*] Reply sent: {coords}")
+            print(f"\u001b[34m [*] \u001b[0m Reply sent: {coords}")
     if(data == "quit"):
         break
 
